935bc9171396a7c32127657dda97090e9dbcc7f5
abolipathak <aboli.pathak98@gmail.com>
2022-11-22 11:13:50 +0000

    Uploading practical-7 part 2

---
+++ Practical-07/find_e.c
@@ -0,0 +1,51 @@
+//Find e using Taylor series expansion  
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int factorial(int n);
+
+int main(void){
+
+    int i, order;
+    double e, *a;
+
+//Enter the order of Polynomial 
+    printf("Please enter the required polynomial order\n");
+    if (scanf("%d", &order) !=1){
+        printf("Error, no number entered");
+        return 1;
+    }
+
+//Allocate space for a array
+    a=malloc(order*sizeof(double));
+    for (i=0; i<order; i++){
+        a[i]=1.0/(double)factorial(i+1);
+        printf("Value of e for order %d is %1.14lf\n", order, a[i]);
+    }
+
+    e=1.0;
+    for(i=0; i<order; i++){
+        e=e+a[i];
+    }
+
+    free(a);
+
+    printf("\ne is estimated as %1.14lf, with difference %1.14lf\n", e, e-exp(1.0));
+
+    return 0;
+}
+
+int factorial(int n){
+    if(n<0){
+        printf("Error: Input should be positive value ");
+        return(-1);
+    }
+    else if (n==0){
+        return 1;
+    }
+    else{
+        return(n*factorial(n-1));
+    }
+}
