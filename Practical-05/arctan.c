011df5e30205eebc3b64d8fe282e6a70c7d9c9de
abolipathak <aboli.pathak98@gmail.com>
2022-11-22 19:28:15 +0000

    adding comments

--- Practical-05/arctan.c
+++ Practical-05/arctan.c
@@ -8,6 +8,7 @@ double arctanh2 (const double x);
 int main(){
 
     double delta, x;
+    //User input for value of delta 
     printf ("Enter the precision for Maclaurin series\n");
     scanf ("%lf", &delta) ;
     
@@ -16,7 +17,7 @@ int main(){
     double tan2[length]; //Store the resuls of arctanh2 for each x
 
     int j=0;//array index 
-    x=-0.9;
+    x=-0.9; 
     while (x<=0.9 && j<length){
     tan1 [j]= arctanh1 (x, delta); 
     tan2 [j]= arctanh2 (x);
@@ -32,13 +33,15 @@ double arctanh1 (const double x, const double delta) {
     double arcTan =0; // approximated result 
     double elem, val;
     int n=0; //sum parameter
+
+//Executes the loop contents once and then checks the condition for re-execution of loop
 do {
 val=2*n+1;
 elem=-pow(x, val) /val; 
 arcTan +=elem;
 n++;
 } 
-
+//fabs(double) takes the absolute value 
 while(fabs (elem) >= delta);
 return arcTan;
 
